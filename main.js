5d5b4ef264ba781eca3089d8c237fc3733f72ace
Vincent Fretin <vincent.fretin@gmail.com>
2022-12-10 10:42:56 +0100

    Don't use scene.renderer.xr.getCamera here, not needed, and this fixes movement issues with movement-controls

--- main.js
+++ main.js
@@ -59,8 +59,7 @@ AFRAME.registerComponent("xr-follow", {
   },
   tick() {
     const scene = this.el.sceneEl;
-    const cameraObject = scene.camera;
-    const camera = scene.is('vr-mode') ? scene.renderer.xr.getCamera(cameraObject) : cameraObject;
+    const camera = scene.camera;
     const object3D = this.el.object3D;
     camera.getWorldPosition(object3D.position);
     object3D.parent.worldToLocal(object3D.position);
